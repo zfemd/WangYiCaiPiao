5babc0f61cac6d4b8f7009d2920e3d1bba9d3451
zy <zhangyangmxjc@163.com>
2015-09-15 22:28:28 +0800

    初始化项目

---
+++ WangYiCaiPiao/WangYiCaiPiao/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WangYiCaiPiao
+//
+//  Created by JamesZY on 15/9/15.
+//  Copyright (c) 2015年 JamesZY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ WangYiCaiPiao/WangYiCaiPiao/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WangYiCaiPiao
+//
+//  Created by JamesZY on 15/9/15.
+//  Copyright (c) 2015年 JamesZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
